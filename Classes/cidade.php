84e4d2a76473c3e431b0c285a4bce89760f9e86f
cleofassoares <cleofassoares@gmail.com>
2024-03-14 20:11:55 -0300

    Classe Cidades Add

--- Classes/cidade.php
+++ Classes/cidade.php
@@ -1,13 +1,23 @@
-$<?php
+<?php
 
-class cidade 
+class Cidade
 {
-private string $nome;
-private string $estado;
-public function __construct(string $nome, string $estado)
+    private string $nome;
+    private string $estado;
 
+    public function __construct($nome, $estado)
+    {
+        $this-> nome = $nome;
+        $this-> estado = $estado;
+    }
 
-{
-    
-}
+    public function getNome() : string
+    {
+        return $this->nome;
+    }
+    public function getEstado() : string
+    {
+        return $this->estado;
+    }
+
 }
